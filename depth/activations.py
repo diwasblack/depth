Fix for softmax function

depth/activations.py
@@ -44,5 +44,5 @@ def softmax_function(x):
     http://cs231n.github.io/linear-classify/#softmax
     """
 
-    e_x = np.exp(x - np.max(x))
+    e_x = np.exp(x - np.max(x, axis=0))
     return e_x / e_x.sum(axis=0)
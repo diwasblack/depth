d0ffb66096159fb15627b4a9dc3a391fb0fc57f0
Diwas Sharma <diwasblack@gmail.com>
2018-06-16 08:16:25 -0500

    Updated setup.py file

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 
 setup(
     name='depth',
-    version='dev',
+    author='Diwas Sharma',
+    author_email='diwasblack@gmail.com',
+    version='0.0.dev0',
     packages=['depth', ],
     license='MIT',
     install_requires=['numpy>=1.14.5'],

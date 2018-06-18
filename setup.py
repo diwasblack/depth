8a6bdc074f3829a075acea2aacd5e2edba25567d
Diwas Sharma <diwasblack@gmail.com>
2018-06-18 11:18:36 -0500

    Updated setup.py to use setuptools instead of distutils

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='depth',
     author='Diwas Sharma',
     author_email='diwasblack@gmail.com',
     version='0.0.dev0',
-    packages=['depth', ],
+    packages=find_packages(),
     license='MIT',
     install_requires=['numpy>=1.14.5'],
 )

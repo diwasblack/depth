d0ce23aef7a1659d86e061b860c051b40c9c9b55
Diwas Sharma <diwasblack@gmail.com>
2018-06-25 21:19:44 -0500

    Fix for kwargs in tanh, relu, sigmoid and linear layers

--- depth/layers.py
+++ depth/layers.py
@@ -10,16 +10,16 @@ from .activations import (
 
 
 class TanhLayer(NonLinearBackprop, LayerBase, XavierWeightInitializer):
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Assign hyperbolic_tangent function to be the activation function
         self.activation_function = hyperbolic_tangent
         self.activation_function_derivative = hyperbolic_tangent_derivative
 
 
 class ReluLayer(NonLinearBackprop, LayerBase, HeWeightInitializer):
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Assign relu function to be the activation function
         self.activation_function = relu
         self.activation_function_derivative = relu_derivative
@@ -35,15 +35,15 @@ class LeakyReluLayer(NonLinearBackprop, LayerBase, HeWeightInitializer):
 
 
 class SigmoidLayer(NonLinearBackprop, LayerBase, XavierWeightInitializer):
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Assign sigmoid function to be the activation function
         self.activation_function = sigmoid_function
         self.activation_function_derivative = sigmoid_function_derivative
 
 
 class LinearLayer(LinearBackprop, LayerBase):
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Return the input as without modification
         self.activation_function = lambda x: x

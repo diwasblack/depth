52d6e316c964dc1b7c4d6b7ce2ce034729449852
Diwas Sharma <diwasblack@gmail.com>
2018-07-09 10:44:03 -0500

    Moved class for DenseLayer to dense_layers.py

--- depth/dense_layers.py
+++ depth/dense_layers.py
@@ -59,3 +59,44 @@ class SoftmaxLayer(LinearBackprop, DenseLayerBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.activation_function = softmax_function
+
+
+class DenseLayer():
+    def __init__(self, activation_function="tanh", units=64,
+                 input_dimension=None, **kwargs):
+        self.activation_function = activation_function
+        self.units = units
+        self.input_dimension = input_dimension
+
+        self.kwargs = kwargs
+
+    def get_layer_obj(self, previous_units=None):
+        activation_function = self.activation_function
+        units = self.units
+        kwargs = self.kwargs
+
+        if(not(previous_units)):
+            previous_units = self.input_dimension
+
+        if(activation_function == "tanh"):
+            layer = TanhLayer(previous_units, units, **kwargs)
+
+        elif(activation_function == "relu"):
+            layer = ReluLayer(previous_units, units, **kwargs)
+
+        elif(activation_function == "leakyrelu"):
+            layer = LeakyReluLayer(previous_units, units, **kwargs)
+
+        elif(activation_function == "sigmoid"):
+            layer = SigmoidLayer(previous_units, units, **kwargs)
+
+        elif(activation_function == "linear"):
+            layer = LinearLayer(previous_units, units, **kwargs)
+
+        elif(activation_function == "softmax"):
+            layer = SoftmaxLayer(previous_units, units, **kwargs)
+
+        else:
+            raise Exception("Unknown layer name received")
+
+        return layer

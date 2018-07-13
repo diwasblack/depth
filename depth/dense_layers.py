b2f6e5af7524880e695769f231ec0c1c8c81eeac
Diwas Sharma <diwasblack@gmail.com>
2018-07-12 20:42:07 -0500

    Moved weight initializers to seperate file

--- depth/dense_layers.py
+++ depth/dense_layers.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 
 from .activations import (
@@ -8,29 +6,8 @@ from .activations import (
     leaky_relu, leaky_relu_derivative,
     sigmoid_function, sigmoid_function_derivative,
     softmax_function)
-
-
-def HeWeightInitializer(input_units, output_units):
-    """
-    An implementation of He weight initialization
-
-    See:
-    https://www.cv-foundation.org/openaccess/content_iccv_2015/papers/He_Delving_Deep_into_ICCV_2015_paper.pdf
-    """
-
-    return np.random.rand(output_units, input_units) * \
-        math.sqrt(2.0 / input_units)
-
-
-def XavierWeightInitializer(input_units, output_units):
-    """
-    An implementation of Xavier weight initialization
-
-    See:
-    http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf
-    """
-    return np.random.rand(output_units, input_units) * \
-        math.sqrt(1.0 / input_units)
+from .initializers import (
+    HeWeightInitializer, XavierWeightInitializer)
 
 
 class DenseLayerBase():
---
+++ depth/initializers.py
@@ -0,0 +1,26 @@
+import math
+
+import numpy as np
+
+
+def HeWeightInitializer(input_units, output_units):
+    """
+    An implementation of He weight initialization
+
+    See:
+    https://www.cv-foundation.org/openaccess/content_iccv_2015/papers/He_Delving_Deep_into_ICCV_2015_paper.pdf
+    """
+
+    return np.random.rand(output_units, input_units) * \
+        math.sqrt(2.0 / input_units)
+
+
+def XavierWeightInitializer(input_units, output_units):
+    """
+    An implementation of Xavier weight initialization
+
+    See:
+    http://proceedings.mlr.press/v9/glorot10a/glorot10a.pdf
+    """
+    return np.random.rand(output_units, input_units) * \
+        math.sqrt(1.0 / input_units)

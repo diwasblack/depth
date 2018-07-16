6fc26120f941bc03459931a393959054f93f6a69
Diwas Sharma <diwasblack@gmail.com>
2018-07-15 20:23:24 -0500

    Using numpy pad function to pad the entire data tensor

--- depth/convolution.py
+++ depth/convolution.py
@@ -52,12 +52,17 @@ def convolve_tensors(data_tensor, kernel_tensor):
     result = np.zeros((number_of_data, number_of_filters,
                        y, x), dtype=np.float32)
 
-    for c in range(channels):
-        logging.debug(
-            "Convolution for channel : {}".format(c+1))
+    # Pad the data with zeros
+    pads = [(0, 0), (0, 0), (1, x), (1, y)]
+    padded_tensor = np.pad(
+        data_tensor, pads, mode="constant", constant_values=0)
 
-        for f in range(number_of_filters):
-            logging.debug("Convolution for filter {}".format(f+1))
+    for f in range(number_of_filters):
+        logging.debug("Convolution for filter {}".format(f+1))
+
+        for c in range(channels):
+            logging.debug(
+                "Convolution for channel : {}".format(c+1))
 
             kernel = kernel_tensor[f, c, :, :]
 
@@ -65,16 +70,6 @@ def convolve_tensors(data_tensor, kernel_tensor):
                 for j in range(x):
                     # Process data one at a time
                     for index in range(number_of_data):
-
-                        data = data_tensor[index, c, :, :]
-
-                        # Zero pad the data
-                        stacked_data = np.hstack(
-                            (np.zeros((x, 1)), data, np.zeros((x, 1))))
-
-                        stacked_data = np.vstack(
-                            (np.zeros((1, y+2)), stacked_data, np.zeros((1, y+2))))
-
                         result[index, f, i, j] += np.sum(
-                            stacked_data[i:i+m, j:j+m]*kernel)
+                            padded_tensor[index, c, i:i+m, j:j+m]*kernel)
     return result
